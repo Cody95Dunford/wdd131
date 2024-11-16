1d8ee87722431b1d7bd76ef717166b7212d7f775
Cody95Dunford <cddunford@byui.edu>
2024-11-16 17:23:50 -0600

    finish temple.html

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,18 +1,28 @@
-function addScripture()
+const button = document.querySelector('button');
+
+button.addEventListener("click", function()
 {
     const input = document.querySelector('#favchap');
-    const button = document.querySelector('button');
+    
     const list = document.querySelector('ul');
-
-	const li = document.createElement('li');
     const deleteButton = document.createElement('button');
-	
+    const li = document.createElement('li');    
+
+    if (input.value.trim() !== '')
+    {
 	li.textContent = input.value;
 	deleteButton.textContent = "❌";
-	
 	li.append(deleteButton);
-	
 	list.append(li);
+    }
 
     input.value = "";
-}
+    input.focus();
+
+    deleteButton.addEventListener("click", function()
+    {
+        list.removeChild(li);
+        input.value = "";
+        input.focus();
+    });
+});
